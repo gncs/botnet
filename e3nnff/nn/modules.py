0c57a71985a06cf3c567ca3b9f8c84a2928b3d6e
Gregor Simm <gncsimm@gmail.com>
2021-06-18 18:52:02 +0100

    add atomic energies module to modules.py

---
+++ e3nnff/nn/modules.py
@@ -0,0 +1,19 @@
+from typing import Union
+
+import numpy as np
+import torch
+
+
+class AtomicEnergiesBlock(torch.nn.Module):
+    def __init__(self, atomic_energies: Union[np.ndarray, torch.Tensor]):
+        super().__init__()
+        assert len(atomic_energies.shape) == 1
+
+        t = torch.tensor(atomic_energies, dtype=torch.get_default_dtype())  # [n_elements, ]
+        self.register_buffer('atomic_energies', t)
+
+    def forward(
+            self,
+            x: torch.Tensor  # one-hot of elements [..., n_elements]
+    ) -> torch.Tensor:  # [..., ]
+        return torch.matmul(x, self.atomic_energies)
--- tests/test_nn.py
+++ tests/test_nn.py
@@ -1,7 +1,31 @@
+import numpy as np
 import torch
+import torch_scatter
 
+from e3nnff.atomic_data import AtomicData, get_data_loader
+from e3nnff.data import Configuration
 from e3nnff.nn.cutoff import PolynomialCutoff
+from e3nnff.nn.modules import AtomicEnergiesBlock
 from e3nnff.nn.radial_basis import BesselBasis
+from e3nnff.tensor_tools import to_numpy
+from e3nnff.utils import AtomicNumberTable
+
+config = Configuration(
+    atomic_numbers=np.array([8, 1, 1]),
+    positions=np.array([
+        [0.0, -2.0, 0.0],
+        [1.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0],
+    ]),
+    forces=np.array([
+        [0.0, -1.3, 0.0],
+        [1.0, 0.2, 0.0],
+        [0.0, 1.1, 0.3],
+    ]),
+    energy=-1.5,
+)
+
+table = AtomicNumberTable([1, 8])
 
 
 class TestBasis:
@@ -17,4 +41,20 @@ class TestCutoff:
         d = torch.linspace(start=0.5, end=5.5, steps=10)
         cutoff_fn = PolynomialCutoff(r_max=5.0)
         output = cutoff_fn(d)
-        assert output.shape == (10, )
+        assert output.shape == (10,)
+
+
+class TestAtomicEnergies:
+    def test_simple(self):
+        energies_block = AtomicEnergiesBlock(atomic_energies=np.array([1.0, 3.0]))
+
+        data1 = AtomicData.from_config(config, table=table, cutoff=3.0)
+        data2 = AtomicData.from_config(config, table=table, cutoff=3.0)
+
+        data_loader = get_data_loader([data1, data2], batch_size=2)
+        batch = next(iter(data_loader))
+
+        energies = energies_block(batch.node_attrs)
+        out = torch_scatter.scatter(src=energies, index=batch.batch, dim=-1, reduce='sum')
+        out = to_numpy(out)
+        assert np.allclose(out, np.array([5., 5.]))
