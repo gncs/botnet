7ca5431108b3781a5f14e40610bbf28e2ff38813
Gregor Simm <gncsimm@gmail.com>
2021-06-18 22:47:44 +0100

    first attempt at interaction block

---
+++ e3nnff/e3nn_tools.py
@@ -0,0 +1,15 @@
+from e3nn import o3
+
+
+def node_edge_combined_irreps(node_irreps: o3.Irreps, edge_irreps: o3.Irreps, out_irreps: o3.Irreps) -> o3.Irreps:
+    combined = []
+    for i, (mul, ir_in) in enumerate(node_irreps):
+        for j, (_, ir_edge) in enumerate(edge_irreps):
+            for ir_out in ir_in * ir_edge:
+                if ir_out in out_irreps:
+                    combined.append((mul, ir_out))
+
+    combined = o3.Irreps(combined)
+    combined, p, _ = combined.sort()
+
+    return combined
--- e3nnff/nn/modules.py
+++ e3nnff/nn/modules.py
@@ -2,6 +2,10 @@ from typing import Union
 
 import numpy as np
 import torch
+from e3nn import o3
+from torch_scatter import scatter
+
+from e3nnff.e3nn_tools import node_edge_combined_irreps
 
 
 class AtomicEnergiesBlock(torch.nn.Module):
@@ -19,3 +23,37 @@ class AtomicEnergiesBlock(torch.nn.Module):
             x: torch.Tensor  # one-hot of elements [..., n_elements]
     ) -> torch.Tensor:  # [..., ]
         return torch.matmul(x, self.atomic_energies)
+
+
+class InteractionBlock(torch.nn.Module):
+    def __init__(
+        self,
+        node_feats_irreps_in: o3.Irreps,
+        node_attrs_irreps_in: o3.Irreps,
+        edge_feats_irreps_in: o3.Irreps,
+        node_feats_irreps_out: o3.Irreps,
+    ) -> None:
+        super().__init__()
+
+        mid_irreps = node_edge_combined_irreps(node_feats_irreps_in, edge_feats_irreps_in, node_feats_irreps_out)
+        self.conv_tp = o3.FullyConnectedTensorProduct(node_feats_irreps_in, edge_feats_irreps_in, mid_irreps)
+        self.linear = o3.Linear(mid_irreps, node_feats_irreps_out)
+        self.skip_tp = o3.FullyConnectedTensorProduct(node_feats_irreps_out, node_attrs_irreps_in,
+                                                      node_feats_irreps_out)
+
+    def forward(
+        self,
+        node_feats: torch.Tensor,
+        node_attrs: torch.Tensor,
+        edge_feats: torch.Tensor,
+        edge_index: torch.Tensor,
+    ) -> torch.Tensor:
+        sender, receiver = edge_index
+        num_nodes = node_feats.shape[0]
+
+        edge_info = self.conv_tp(node_feats[sender], edge_feats)
+        x = scatter(edge_info, index=receiver, dim=0, dim_size=num_nodes, reduce='sum')
+        x = self.linear(x)
+        x_skip = self.skip_tp(x, node_attrs)
+
+        return x + x_skip
