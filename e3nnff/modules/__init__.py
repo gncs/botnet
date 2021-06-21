886c84216756b2ba522727ee5e90d10258c53db2
Gregor Simm <gncsimm@gmail.com>
2021-06-21 17:25:42 +0100

    add more code to EnergyForcesLoss

--- e3nnff/modules/__init__.py
+++ e3nnff/modules/__init__.py
@@ -1,8 +1,9 @@
 from .blocks import AtomicEnergiesBlock, SkipInteractionBlock, EdgeEmbeddingBlock, LinearReadoutBlock, ScaleShiftBlock
 from .cutoff import PolynomialCutoff
 from .radial_basis import BesselBasis
+from .loss import EnergyForcesLoss
 
 __all__ = [
     'AtomicEnergiesBlock', 'SkipInteractionBlock', 'EdgeEmbeddingBlock', 'LinearReadoutBlock', 'ScaleShiftBlock',
-    'PolynomialCutoff', 'BesselBasis'
+    'PolynomialCutoff', 'BesselBasis', 'EnergyForcesLoss'
 ]
--- e3nnff/modules/loss.py
+++ e3nnff/modules/loss.py
@@ -1,16 +1,18 @@
-from typing import Tuple
-
 import torch
 from torch_geometric.data import Batch
 
 from e3nnff.tools import TensorDict
 
 
-class EnergyForceLoss(torch.nn.Module):
-    def __init__(self, energy_weight=1.0, force_weight=1.0) -> None:
+def mean_squared_error_energy(batch: Batch, predictions: TensorDict) -> torch.Tensor:
+    return torch.mean(torch.square(batch['energy'] - predictions['energy']))
+
+
+class EnergyForcesLoss(torch.nn.Module):
+    def __init__(self, energy_weight=1.0, forces_weight=1.0) -> None:
         super().__init__()
         self.register_buffer('energy_weight', torch.tensor(energy_weight, dtype=torch.get_default_dtype()))
-        self.register_buffer('force_weight', torch.tensor(force_weight, dtype=torch.get_default_dtype()))
+        self.register_buffer('forces_weight', torch.tensor(forces_weight, dtype=torch.get_default_dtype()))
 
-    def forward(self, batch: Batch, predictions: TensorDict) -> Tuple[torch.Tensor, TensorDict]:
-        raise NotImplementedError
+    def forward(self, batch: Batch, predictions: TensorDict) -> torch.Tensor:
+        return mean_squared_error_energy(batch, predictions)
