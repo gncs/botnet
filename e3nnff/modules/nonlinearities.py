5e1fa0b84c8872d3a4816889ca1b4fc12fd0cad0
Gregor Simm <gncsimm@gmail.com>
2021-07-08 16:21:32 +0100

    add type annotations

--- e3nnff/modules/nonlinearities.py
+++ e3nnff/modules/nonlinearities.py
@@ -3,10 +3,10 @@ import torch
 
 
 class ShiftedSoftPlus(torch.nn.Module):
-    def __init__(self, beta=1, threshold=20):
+    def __init__(self, beta=1, threshold=20) -> None:
         super().__init__()
         self.softplus = torch.nn.Softplus(beta=beta, threshold=threshold)
         self.log2 = np.log(2.0)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.softplus(x) - self.log2
