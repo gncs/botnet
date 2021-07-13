0862648f04f10036b33123ffbd20092ee76d13cc
Gregor Simm <gncsimm@gmail.com>
2021-07-13 17:05:47 +0100

    make bessel functions learnable

--- e3nnff/modules/radial.py
+++ e3nnff/modules/radial.py
@@ -7,12 +7,16 @@ class BesselBasis(torch.nn.Module):
     Klicpera, J.; Groß, J.; Günnemann, S. Directional Message Passing for Molecular Graphs; ICLR 2020.
     Equation (7)
     """
-    def __init__(self, r_max: float, num_basis=8):
+    def __init__(self, r_max: float, num_basis=8, trainable=False):
         super().__init__()
 
         bessel_weights = np.pi / r_max * torch.linspace(start=1.0, end=num_basis, steps=num_basis,
                                                         dtype=torch.get_default_dtype())
-        self.register_buffer('bessel_weights', bessel_weights)
+        if trainable:
+            self.bessel_weights = torch.nn.Parameter(bessel_weights)
+        else:
+            self.register_buffer('bessel_weights', bessel_weights)
+
         self.register_buffer('r_max', torch.tensor(r_max, dtype=torch.get_default_dtype()))
         self.register_buffer('prefactor', torch.tensor(np.sqrt(2.0 / r_max), dtype=torch.get_default_dtype()))
 
@@ -24,7 +28,8 @@ class BesselBasis(torch.nn.Module):
         return self.prefactor * (numerator / x)
 
     def __repr__(self):
-        return f'{self.__class__.__name__}(r_max={self.r_max}, num_basis={len(self.bessel_weights)})'  # type: ignore
+        return f'{self.__class__.__name__}(r_max={self.r_max}, num_basis={len(self.bessel_weights)}, ' \
+               f'trainable={self.bessel_weights.requires_grad})'
 
 
 class PolynomialCutoff(torch.nn.Module):
