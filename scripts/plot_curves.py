ae0fd2dd75b07bf8b7379dbae01d23031e4482e1
Gregor Simm <gncsimm@gmail.com>
2021-08-04 13:24:05 +0200

    remove colors

--- scripts/plot_curves.py
+++ scripts/plot_curves.py
@@ -13,19 +13,6 @@ fig_height = 2.1
 
 plt.rcParams.update({'font.size': 6})
 
-colors = [
-    '#1f77b4',  # muted blue
-    '#d62728',  # brick red
-    '#ff7f0e',  # safety orange
-    '#2ca02c',  # cooked asparagus green
-    '#9467bd',  # muted purple
-    '#8c564b',  # chestnut brown
-    '#e377c2',  # raspberry yogurt pink
-    '#7f7f7f',  # middle gray
-    '#bcbd22',  # curry yellow-green
-    '#17becf',  # blue-teal
-]
-
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
