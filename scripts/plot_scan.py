25d1b507e144cc042a3a69a3279153649926b301
Gregor Simm <gncsimm@gmail.com>
2021-08-07 08:51:51 +0200

    use custom config_from_atoms function (energy, DFT_energy,...)

--- scripts/plot_scan.py
+++ scripts/plot_scan.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 
+import ase.data
 import ase.io
 import matplotlib.pyplot as plt
 import numpy as np
@@ -23,12 +24,28 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def config_from_atoms(atoms: ase.Atoms) -> data.Configuration:
+    energy = atoms.info.get('energy', None)
+    if energy is None:
+        energy = atoms.info.get('DFT_energy')
+
+    energy = float(energy)
+
+    forces = None
+    if atoms.has('forces'):
+        forces = atoms.get_forces()
+
+    atomic_numbers = np.array([ase.data.atomic_numbers[symbol] for symbol in atoms.symbols])
+
+    return data.Configuration(atomic_numbers=atomic_numbers, positions=atoms.positions, energy=energy, forces=forces)
+
+
 def main():
     args = parse_args()
 
     atoms_list = ase.io.read(args.configs_path, format='extxyz', index=':')
-    configs = [data.config_from_atoms(atoms) for atoms in atoms_list]
-    ref_energies = np.array([float(atoms.info.get('DFT_energy')) for atoms in atoms_list], dtype=float)
+    configs = [config_from_atoms(atoms) for atoms in atoms_list]
+    ref_energies = np.array([config.energy for config in configs], dtype=float)
     assert all(energy is not None for energy in ref_energies)
 
     zs = [int(z) for z in args.atomic_numbers.split(',')]
