86ebaaab1cb339f95c8ff132524a5ca2d1a11049
Gregor Simm <gncsimm@gmail.com>
2021-09-01 16:09:23 +0100

    set defaults in argparser

--- e3nnff/tools/arg_parser.py
+++ e3nnff/tools/arg_parser.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 
 def build_default_arg_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(description='Command line tool of MolGen3D')
+    parser = argparse.ArgumentParser()
 
     # Name and seed
     parser.add_argument('--name', help='experiment name', required=True)
@@ -20,21 +20,23 @@ def build_default_arg_parser() -> argparse.ArgumentParser:
                         help='set default dtype',
                         type=str,
                         choices=['float32', 'float64'],
-                        default='float32')
+                        default='float64')
     parser.add_argument('--log_level', help='log level', type=str, default='INFO')
 
     # Model
     parser.add_argument('--r_max', help='distance cutoff (in Ang)', type=float, default=4.0)
     parser.add_argument('--num_radial_basis', help='number of radial basis functions', type=int, default=10)
     parser.add_argument('--num_cutoff_basis', help='number of basis functions for smooth cutoff', type=int, default=8)
-    parser.add_argument('--max_ell', help=r'maximum \ell in spherical harmonics series expansion', type=int, default=2)
-    parser.add_argument('--interaction', help='name of interaction block', type=str, default='SkipInteractionBlock')
-    parser.add_argument('--num_interactions', help='number of interactions', type=int, default=4)
+    parser.add_argument('--max_ell', help=r'maximum \ell in spherical harmonics series expansion', type=int, default=3)
+    parser.add_argument('--interaction',
+                        help='name of interaction block',
+                        type=str,
+                        default='ElementDependentInteractionBlock')
+    parser.add_argument('--num_interactions', help='number of interactions', type=int, default=6)
     parser.add_argument('--hidden_irreps',
                         help='irreps for hidden node states',
                         type=str,
-                        default='10x0e + 10x0o + 10x1e + 10x1o + 10x2e + 10x2o')
-    parser.add_argument('--no_forces', help='no forces in loss function', action='store_true', default=False)
+                        default='32x0e + 32x1o + 32x2e + 32x3o')
 
     # Optimizer
     parser.add_argument('--optimizer',
@@ -44,12 +46,12 @@ def build_default_arg_parser() -> argparse.ArgumentParser:
                         choices=['adam', 'amsgrad'])
     parser.add_argument('--batch_size', help='batch size', type=int, default=10)
     parser.add_argument('--lr', help='Learning rate of optimizer', type=float, default=0.01)
-    parser.add_argument('--lr_scheduler_gamma', help='Gamma of learning rate scheduler', type=float, default=0.9995)
+    parser.add_argument('--lr_scheduler_gamma', help='Gamma of learning rate scheduler', type=float, default=1.0)
     parser.add_argument('--max_num_epochs', help='Maximum number of epochs', type=int, default=2048)
     parser.add_argument('--patience',
                         help='Maximum number of consecutive epochs of increasing loss',
                         type=int,
-                        default=64)
+                        default=2048)
     parser.add_argument('--eval_interval', help='evaluate model every <n> epochs', type=int, default=1)
     parser.add_argument('--keep_checkpoints', help='keep all checkpoints', action='store_true', default=False)
     parser.add_argument('--restart_latest',
