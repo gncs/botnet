8dc2b7f86497b1e2807a672097a9beb1efaa3790
Gregor Simm <gncsimm@gmail.com>
2021-09-01 21:31:49 +0100

    remove xyz.py module

--- e3nnff/data/__init__.py
+++ e3nnff/data/__init__.py
@@ -9,11 +9,9 @@ from .rmd17 import load as load_rmd17
 from .three_bpa import atomic_energies as three_bpa_atomic_energies
 from .three_bpa import load as load_3bpa
 from .utils import Configuration, Configurations, split_train_valid_configs
-from .xyz import load_xyz, config_from_atoms
 
 __all__ = [
     'load_rmd17', 'rmd17_atomic_energies', 'AtomicData', 'get_data_loader', 'get_neighborhood', 'Configuration',
-    'Configurations', 'load_xyz', 'config_from_atoms', 'split_train_valid_configs', 'load_iso17',
-    'iso17_atomic_energies', 'load_3bpa', 'three_bpa_atomic_energies', 'acetylacetone_atomic_energies',
-    'load_acetylacetone'
+    'Configurations', 'split_train_valid_configs', 'load_iso17', 'iso17_atomic_energies', 'load_3bpa',
+    'three_bpa_atomic_energies', 'acetylacetone_atomic_energies', 'load_acetylacetone'
 ]
