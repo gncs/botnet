da764f503c14a371716c8b4b55e57fd6426a95a8
Gregor Simm <gncsimm@gmail.com>
2021-09-03 16:15:07 +0100

    simplify acac parser

--- e3nnff/data/acac.py
+++ e3nnff/data/acac.py
@@ -34,27 +34,19 @@ def config_from_atoms(atoms: ase.Atoms) -> Configuration:
 def unpack_configs(path: str) -> Dict[str, Configurations]:
     logging.info('Unpacking archive')
 
-    subsets = {'train_300K', 'train_600K', 'test_MD_300K', 'test_MD_600K', 'test_dihedral', 'test_H_transfer'}
-    file_subset_dict = {f'{subset}.xyz': subset for subset in subsets}
-
     extracted_data: Dict[str, Configurations] = {}
-
-    # Extract files
     with tarfile.open(name=path, mode='r|gz') as tar_file:
-        # Find files
         for file in tar_file:
             basename = os.path.basename(file.name)
-            if basename in file_subset_dict.keys():
+            root, ext = os.path.splitext(basename)
+            if file.isfile() and ext == '.xyz':
                 extracted_file = tar_file.extractfile(file)
                 if extracted_file:
                     content = io.StringIO(extracted_file.read().decode('ascii'))
                     configs = [config_from_atoms(config) for config in ase.io.read(content, format='extxyz', index=':')]
-                    extracted_data[file_subset_dict[basename]] = configs
+                    extracted_data[root] = configs
                 else:
                     raise RuntimeError(f'Cannot read file: {file.name}')
-
-            if len(extracted_data) == len(file_subset_dict):
-                break
 
     return extracted_data
 
