f905e8aedb82f4d57b4131be159f2a5209c248a9
Gregor Simm <gncsimm@gmail.com>
2021-09-07 21:12:32 +0100

    add first 3bpa plot and plot settings

---
+++ scripts/plot_settings.py
@@ -0,0 +1,23 @@
+colors = [
+    '#1f77b4',  # muted blue
+    '#d62728',  # brick red
+    '#ff7f0e',  # safety orange
+    '#2ca02c',  # cooked asparagus green
+    '#9467bd',  # muted purple
+    '#8c564b',  # chestnut brown
+    '#e377c2',  # raspberry yogurt pink
+    '#7f7f7f',  # middle gray
+    '#bcbd22',  # curry yellow-green
+    '#17becf',  # blue-teal
+]
+
+style_dict = {
+    'dft': {
+        'color': 'black',
+        'label': 'DFT',
+    },
+    'botnet': {
+        'color': colors[0],
+        'label': 'BOTNet',
+    }
+}
