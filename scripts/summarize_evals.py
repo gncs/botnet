31f745ff15bcb16e8aca29666e0b3a867c44fa27
Gregor Simm <gncsimm@gmail.com>
2021-09-10 12:08:34 +0100

    further simplify summarize_evals.py script

--- scripts/summarize_evals.py
+++ scripts/summarize_evals.py
@@ -1,9 +1,7 @@
 import argparse
-import dataclasses
 import glob
 import json
 import os
-import re
 from typing import List, Dict
 
 import numpy as np
@@ -20,35 +18,9 @@ def get_paths(directory: dir) -> List[str]:
     return glob.glob(os.path.join(directory, '*_eval.txt'))
 
 
-@dataclasses.dataclass
-class ExperimentInfo:
-    name: str
-    seed: int
-
-
-name_re = re.compile(r'(?P<name>.+)_run-(?P<seed>\d+)_eval.txt')
-
-
-def parse_path(path: str) -> ExperimentInfo:
-    match = name_re.match(os.path.basename(path))
-    if not match:
-        raise RuntimeError(f'Cannot parse {path}')
-
-    return ExperimentInfo(name=match.group('name'), seed=int(match.group('seed')))
-
-
 def read_results(path: str) -> List[Dict]:
-    exp_info = parse_path(path)
-
-    results = []
     with open(path, mode='r') as f:
-        for line in f.readlines():
-            result = json.loads(line)
-            result['exp'] = exp_info.name
-            result['seed'] = exp_info.seed
-            results.append(result)
-
-    return results
+        return [json.loads(line) for line in f.readlines()]
 
 
 def main():
@@ -56,12 +28,12 @@ def main():
     results_list = [result for path in get_paths(directory=args.dir) for result in read_results(path)]
     df = pd.DataFrame(results_list)
 
-    df = df.drop(columns=['seed'])
-    df = df.groupby(['exp', 'name']).agg([np.mean, np.std])
-
     converted = ['mae_e', 'mae_f', 'rmse_e', 'rmse_f']
     df = df.apply(lambda column: column * 1000 if column.name in converted else column)
     print(f'Columns {", ".join(converted)} are in meV or meV/Ang')
+
+    df = df.groupby(['name', 'subset']).agg([np.mean, np.std])
+    df = df.drop(columns=['seed'])
 
     with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.expand_frame_repr', False,
                            'display.float_format', '{:.3f}'.format):
