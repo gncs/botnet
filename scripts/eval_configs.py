09aff307a0c0ace64a92242e345881e10c542584
Gregor Simm <gncsimm@gmail.com>
2021-09-19 19:33:10 +0100

    store predicted forces as well

--- scripts/eval_configs.py
+++ scripts/eval_configs.py
@@ -35,6 +35,7 @@ def main():
     tools.set_default_dtype(args.default_dtype)
     device = tools.init_device(args.device)
 
+    # Load data and prepare input
     atoms_list = ase.io.read(args.configs, format='extxyz', index=':')
     configs = [config_from_atoms(atoms) for atoms in atoms_list]
 
@@ -47,25 +48,34 @@ def main():
         drop_last=False,
     )
 
+    # Load model
     model: modules.BodyOrderedModel = torch.load(f=args.model, map_location=device)
 
+    # Collect data
     energies_list = []
     contributions_list = []
+    forces_collection = []
+
     for batch in data_loader:
         batch = batch.to(device)
         output = model(batch, training=False)
         energies_list.append(tools.to_numpy(output['energy']))
         contributions_list.append(tools.to_numpy(output['contributions']))
 
+        forces = np.split(tools.to_numpy(output['forces']), indices_or_sections=batch.ptr[1:], axis=0)
+        forces_collection.append(forces[:-1])  # drop last as its emtpy
+
     energies = np.concatenate(energies_list, axis=0)
     contributions = np.concatenate(contributions_list, axis=0)
+    forces_list = [forces for forces_list in forces_collection for forces in forces_list]
 
-    # Overwrite info dict
-    assert len(energies) == len(atoms_list) == contributions.shape[0]
-    for atoms, energy, contribution in zip(atoms_list, energies, contributions):
+    # Store data in atoms objects
+    assert len(atoms_list) == len(energies) == contributions.shape[0] == len(forces_list)
+    for atoms, energy, contribution, forces in zip(atoms_list, energies, contributions, forces_list):
         atoms.calc = None  # crucial
         atoms.info['energy'] = energy
         atoms.info['contributions'] = contribution
+        atoms.arrays['forces'] = forces
 
     # Write atoms to output path
     ase.io.write(args.output, images=atoms_list, format='extxyz')
