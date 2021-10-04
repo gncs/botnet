24795fd551daf24192d830b0012763143f1a9d79
Gregor Simm <gncsimm@gmail.com>
2021-10-04 14:44:19 +0100

    add histogram to ethanol plot

--- scripts/plot_ethanol.py
+++ scripts/plot_ethanol.py
@@ -4,6 +4,7 @@ from typing import Tuple
 import ase.data
 import ase.io
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from matplotlib import ticker
 
@@ -14,10 +15,18 @@ plt.rcParams.update({'font.size': 6})
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
+    parser.add_argument('--train_configs', required=True)
     parser.add_argument('--removal_configs', action='append', required=True)
     parser.add_argument('--soft_configs', action='append', required=True)
     parser.add_argument('--hard_configs', action='append', required=True)
     return parser.parse_args()
+
+
+def parse_train_configs(path: str) -> pd.DataFrame:
+    atoms_list = ase.io.read(path, format='extxyz', index=':')
+    return pd.DataFrame({
+        'distance': [atoms.info['OH_dist'] for atoms in atoms_list],
+    })
 
 
 def parse_removal_configs(parse_removal_path: str) -> Tuple[str, pd.DataFrame]:
@@ -40,50 +49,69 @@ def parse_displacement_path(name_path_tuple: str) -> Tuple[str, pd.DataFrame]:
 
 def main():
     args = parse_args()
+    training = parse_train_configs(args.train_configs)
     removal_predictions = [parse_removal_configs(path) for path in args.removal_configs]
     soft_predictions = [parse_displacement_path(path) for path in args.soft_configs]
     hard_predictions = [parse_displacement_path(path) for path in args.hard_configs]
 
-    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(6.0, 1.5), constrained_layout=True, sharey='row')
+    fig, axes = plt.subplots(nrows=2,
+                             ncols=3,
+                             figsize=(6.0, 2.0),
+                             constrained_layout=True,
+                             sharey='row',
+                             sharex='col',
+                             gridspec_kw={'height_ratios': (5, 1)})
 
-    # Removal
-    ax = axes[0]
+    # Removal energies
+    ax = axes[0, 0]
     for name, df in removal_predictions:
-        ax.plot(df['distance'], df['energy'], **style_dict[name])
+        selection = df[df['distance'] <= 5.0]
+        ax.plot(selection['distance'], selection['energy'], **style_dict[name])
 
-    ax.set_xticks([0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0])
-    ax.set_xlabel('Distance [Å]')
+    ax.set_xticks([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
 
     ax.yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.0f}"))
     ax.xaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.1f}"))
 
-    ax.set_title(r'Hydrogen Abstraction')
+    ax.set_title('Hydrogen Abstraction')
     ax.set_ylabel(r'$E$ [eV]')
     ax.legend()
 
+    # Removal histogram
+    ax = axes[1, 0]
+    d_range = np.arange(0.95, 5.0, step=0.025)
+    ax.hist(training['distance'], bins=d_range, color='black', label='Training data')
+    ax.set_ylabel('Count')
+    ax.set_xlabel('Distance [Å]')
+    ax.legend()
+
     # Soft
-    ax = axes[1]
+    ax = axes[0, 1]
     for name, df in soft_predictions:
         ax.plot(df['displacement'], df['energy'], **style_dict[name])
 
     ax.set_title(r'$\tilde{\nu} = 874 \, \mathrm{cm}^{-1}$')
     ax.set_xticks([-1.0, 0.0, 1.0, 2.0])
-    ax.set_xlabel('Displacement')
 
     ax.yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.0f}"))
     ax.xaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.1f}"))
 
+    ax = axes[1, 1]
+    ax.set_xlabel('Displacement')
+
     # Hard
-    ax = axes[2]
+    ax = axes[0, 2]
     for name, df in hard_predictions:
         ax.plot(df['displacement'], df['energy'], **style_dict[name])
 
     ax.set_title(r'$\tilde{\nu} = 3005 \, \mathrm{cm}^{-1}$')
     ax.set_xticks([-1.0, -0.5, 0.0, 0.5])
-    ax.set_xlabel('Displacement')
 
     ax.yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.0f}"))
     ax.xaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.1f}"))
+
+    ax = axes[1, 2]
+    ax.set_xlabel('Displacement')
 
     fig.savefig('ethanol.pdf')
 
