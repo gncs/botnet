504b60a43f445d43358d7044960561c6b1471de6
Ilyes Batatia <48651863+ilyes319@users.noreply.github.com>
2021-12-16 17:44:51 +0100

    removed norm in non linear readout

--- e3nnff/modules/blocks.py
+++ e3nnff/modules/blocks.py
@@ -55,8 +55,7 @@ class NonLinearReadoutBlock(torch.nn.Module):
     def __init__(self, irreps_in: o3.Irreps, MLP_irreps: o3.Irreps, gate: Callable):
         super().__init__()
         self.hidden_irreps = MLP_irreps
-        self.norm = o3.Norm(irreps_in, squared=True)
-        self.linear_1 = o3.Linear(irreps_in=self.norm.irreps_out, irreps_out=self.hidden_irreps)
+        self.linear_1 = o3.Linear(irreps_in=irreps_in, irreps_out=self.hidden_irreps)
         self.non_linearity = nn.Activation(irreps_in=self.hidden_irreps, acts=[gate])
         self.linear_2 = o3.Linear(irreps_in=self.hidden_irreps, irreps_out=o3.Irreps('0e'))
 
@@ -64,7 +63,6 @@ class NonLinearReadoutBlock(torch.nn.Module):
             self,
             x: torch.Tensor  # [n_nodes, irreps]
     ) -> torch.Tensor:  # [..., ]
-        x = self.norm(x)
         x = self.non_linearity(self.linear_1(x))
         return self.linear_2(x)  # [n_nodes, 1]
 
