403bae576fe9e03ad0e928565490b5a5b39d3a85
Ilyes Batatia <48651863+ilyes319@users.noreply.github.com>
2022-06-22 19:35:14 +0200

    Update plot_ethanol.py

--- scripts/plot_ethanol.py
+++ scripts/plot_ethanol.py
@@ -12,6 +12,38 @@ from utils import style_dict
 
 plt.rcParams.update({'font.size': 6})
 
+opt_pos = np.array([[ 0.00832539, -0.0818815,  -0.48916197],
+ [ 0.50161103,  1.19423113,  0.18777597],
+ [-0.83937646, -0.86183849,  0.33615192],
+ [-0.59614022, 0.1767398 , -1.38445053],
+ [ 0.88248886,-0.67200338, -0.86504484],
+ [ 1.14055339,  0.96085354,  1.06685176],
+ [ 1.1082602 ,  1.81342654, -0.50510823],
+ [-0.35458929,  1.802107 ,   0.54342772],
+ [-0.31917678 ,-1.115707  ,  1.12065934]]).flatten()
+ 
+nm874 = np.array([[ 0.0090944, -0.0347656, -0.0836763],
+                [ 0.0849409,  0.1252834, -0.0138433],
+                [-0.0609697, -0.0697392,  0.0465933],
+                [-0.0550655, -0.1223949, -0.0622117],
+                [-0.0649261, -0.1489499, -0.0499826],
+                [-0.1089109, -0.0452769,  0.0848378],
+                [ 0.1408352,  0.4660962,  0.339914 ],
+                [-0.0544728, -0.1141104,  0.0723458],
+                [-0.0101275, -0.0071352,  0.0377034]]).flatten()
+norm874 = np.linalg.norm(nm874)
+
+
+nm3005 = np.array([[-5.179180e-02,  2.430730e-02, -5.683990e-02],
+       [ 9.310500e-03, -1.498160e-02,  1.326170e-02],
+       [ 1.589600e-03,  1.274000e-04,  7.249000e-04],
+       [ 5.015432e-01, -2.133678e-01,  7.274081e-01],
+       [ 1.066781e-01, -8.137340e-02, -5.556380e-02],
+       [-8.356730e-02,  3.236070e-02, -1.138591e-01],
+       [ 8.048020e-02,  7.334830e-02, -8.891410e-02],
+       [-1.111322e-01,  7.494960e-02,  4.920480e-02],
+       [-1.309240e-02,  9.436000e-04, -1.050630e-02]]).flatten()
+norm3005 = np.linalg.norm(nm3005)
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
@@ -26,6 +58,7 @@ def parse_train_configs(path: str) -> pd.DataFrame:
     atoms_list = ase.io.read(path, format='extxyz', index=':')
     return pd.DataFrame({
         'distance': [atoms.info['OH_dist'] for atoms in atoms_list],
+        'positions': [atoms.get_positions().flatten() for atoms in atoms_list],
     })
 
 
@@ -67,7 +100,7 @@ def main():
 
     fig, axes = plt.subplots(nrows=2,
                              ncols=3,
-                             figsize=(6.0, 2.0),
+                             figsize=(6.0, 2.6),
                              constrained_layout=True,
                              sharey='row',
                              sharex='col',
@@ -101,7 +134,7 @@ def main():
     d_range = np.arange(0.95, 5.0, step=0.025)
     ax.hist(training['distance'], bins=d_range, color='black', label='Training data')
     ax.set_ylabel('Count')
-    ax.set_xlabel('Distance [Å]')
+    ax.set_xlabel('Distance')
 
 
     # Soft
@@ -123,16 +156,25 @@ def main():
 
     ax.yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.0f}"))
     ax.xaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.1f}"))
-
+    
+    #Soft histogram
     ax = axes[1, 1]
-    ax.set_xlabel('Displacement')
+    projections = []
+    for position in training['positions'] :
+        disp = position - opt_pos
+        proj = np.dot(disp, nm874) / norm874**2
+        projections.append(proj / (1 + np.linalg.norm(disp - proj * nm3005)))
+
+    ax.hist(projections, color='black', label='Training data')
+    ax.set_xlabel("Displacement")
+    ax.set_ylabel("Count")
 
     # Hard
     ax = axes[0, 2]
     for index, (name, df) in enumerate(hard_predictions):
-        ax.plot(df['displacement'], df['mean_energy'], **style_dict[name])
+        ax.plot(-df['displacement'], df['mean_energy'], **style_dict[name])
         ax.fill_between(
-            x=df['displacement'],
+            x=-df['displacement'],
             y1=df['mean_energy'] - df['std_energy'],
             y2=df['mean_energy'] + df['std_energy'],
             alpha=0.3,
@@ -141,16 +183,25 @@ def main():
         )
 
     ax.set_title(r'$\tilde{\nu} = 3005 \, \mathrm{cm}^{-1}$')
-    ax.set_xticks([-1.0, -0.5, 0.0, 0.5])
+    ax.set_xticks([1.0, 0.5, 0.0, -0.5, -1.0])
 
     ax.yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.0f}"))
     ax.xaxis.set_major_formatter(ticker.StrMethodFormatter("{x:.1f}"))
     ax.legend(bbox_to_anchor=(1.04,1), loc="upper left")
 
+    #Hard histogram
     ax = axes[1, 2]
-    ax.set_xlabel('Displacement')
+    projections = []
+    for position in training['positions'] :
+        disp = opt_pos - position
+        proj = np.dot(disp, nm3005) / norm3005**2
+        projections.append(proj / (1 + np.linalg.norm(disp - proj * nm3005)))
 
-    fig.savefig('ethanol.pdf')
+    ax.hist(projections, color='black', label='Training data')
+    ax.set_xlabel("Displacement")
+    ax.set_ylabel("Count")
+
+    fig.savefig('ethanol_proj.pdf',bbox_inches='tight')
 
 
 if __name__ == '__main__':
